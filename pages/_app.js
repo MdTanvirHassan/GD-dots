871a474c576dc20df33f316be4c291ddd9e01eed
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-23 12:05:26 +0600

    GD Dots

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<>
+  <Navbar/>
+   <Component {...pageProps} />
+   <Footer/>
+   </>
 }
