e25a8c26a2185f4377ef5b7793e6cbf7f149f2b7
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-24 01:35:33 +0600

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -22,7 +22,7 @@ const Services = () => {
       
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Graphics <br /> Design</h2> 
@@ -31,7 +31,7 @@ const Services = () => {
 
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img2} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2"> Motion<br /> Graphics</h2> 
@@ -40,7 +40,7 @@ const Services = () => {
 
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img3} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2">3D <br /> Animation</h2> 
@@ -49,7 +49,7 @@ const Services = () => {
 
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img4} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2">UI/UX <br /> Design</h2> 
@@ -59,7 +59,7 @@ const Services = () => {
 
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img5} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Clipping<br /> Path</h2> 
@@ -68,7 +68,7 @@ const Services = () => {
 
       <div className="xl:w-1/3 md:w-1/2 w-full px-0 py-2  md:px-8">
         <div className="bg-white border border-gray-200 py-2 rounded-lg shadow-xl shadow-gray-400">
-          <div className="w-40  inline-flex items-center justify-center rounded-md bg-indigo-100 text-indigo-500 mb-4">
+          <div className="w-40  inline-flex items-center justify-center rounded-md  mb-4">
             <Image src={img6} className="w-full h-full rounded-lg" alt='..'/>
           </div>
           <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Video <br /> Editing</h2> 
